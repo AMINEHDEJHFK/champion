defe6e1c5ddba58773f993f14e2028ee9cb7e34a
UlrichIvan <mtchokos@gmail.com>
2024-12-13 17:58:07 +0100

    merger backend an frontend done succesfully

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 # Ce module contient les instances de vos classes back et front end.
 from backend.Draw import Draw
+from frontend.pdf import PDF
+from utils import load_json, teams_path_json, tirages_path_json
 
-# from frontend.pdf import PDF
-# from utils import load_json, teams_path_json
-
-# load samples success results from Draw
-# json_tirages = load_json(path="data/tirage.json")
-
-# json_teams = load_json(path=teams_path_json)
-
-# pdf = PDF(tirages=json_tirages,json_teams=json_teams)
-
-# pdf.generate()
-# pdf.export()
-
-from utils import teams_path_json, load_json
-
-# load teams from json file
+# 1.make draw
 
 json_teams = load_json(path=teams_path_json)
 
@@ -29,3 +16,14 @@ list_pots = Draw.get_list_of_pots(dict_pots.copy())
 draw = Draw(pots=list_pots.copy())
 
 draw.make_draw()
+
+# 2. generate pdf after draw
+
+# load samples success results from Draw
+json_tirages = load_json(path=tirages_path_json)
+
+pdf = PDF(tirages=json_tirages, json_teams=json_teams)
+
+pdf.generate()
+
+pdf.export()
